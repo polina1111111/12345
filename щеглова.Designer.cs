91cf7dc36a23e3899758730762bcde9880440778
Юзер <user@d3k.kuzstu.ru>
2022-10-22 13:51:18 +0700

    2

--- щеглова.Designer.cs
+++ щеглова.Designer.cs
@@ -96,7 +96,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.кнопочка);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "text";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
